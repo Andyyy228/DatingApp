c648b4db8574d12ab9a16c3c4db3540dfccd36aa
Andyyy228 <anton.bot1212@gmail.com>
2022-11-25 02:48:13 +0100

    End of section 19

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -15,6 +15,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -22,9 +23,11 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   sendMessage() {
+    if (!this.username) return;
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm.reset();
-    })
+    }).finally(() => this.loading = false); 
   }
 
 }
